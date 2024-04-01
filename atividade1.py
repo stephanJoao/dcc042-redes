ff460fa0fd6770f6b8bd06bc0cd775b1a6d0c74d
Joao Stephan <joaossmauricio@gmail.com>
2024-04-01 16:38:40 -0300

    pronto para experimentos

--- atividade1.py
+++ atividade1.py
@@ -1,14 +1,12 @@
 import datetime
 import numpy as np
 
-probErroE = 0.2 # probabilidade de mensagem ser corrompida
-probErroF = 0.2 # probabilidade de mensagem ser perdida
-
 numeroErrosE = 0
 numeroErrosF = 0
 contadorMensagens = 0
+totalMensagens = 1e6
 
-def escuto():
+def escuto(probErroE=0.1, probErroF=0.1):
 	global numeroErrosE, numeroErrosF
 	rand = np.random.uniform()
 	if rand < probErroE:
@@ -28,27 +26,53 @@ def espera():
 		if (datetime.datetime.now() - inicio).total_seconds() > tempo:
 			break
 
-while True:			
-	# escuto
-	estadoDaComunicacao = escuto()
-	# se silêncio -> transmite
-	if estadoDaComunicacao == "silencio":
-		print(f"mensagem {contadorMensagens} de dado enviada")
-	# se não -> espera
-	else:
-		print(f"mensagem {contadorMensagens} de dados não recebida")
-		espera()
-		continue
+def simulacao(totalMensagens=1e6, probErroE=0.1, probErroF=0.1, esperar=False, imprime=False):
+	contadorMensagens = 0
+	while True and contadorMensagens < totalMensagens:			
+		# escuto
+		estadoDaComunicacao = escuto(probErroE=probErroE, probErroF=probErroF)
+		# se silêncio -> transmite
+		if estadoDaComunicacao == "silencio":
+			if imprime:
+				print(f"mensagem {contadorMensagens} de dado enviada")
+		# se não -> espera
+		else:
+			if imprime:
+				print(f"mensagem {contadorMensagens} de dado não enviada")
+			if esperar:
+				espera()
+			continue
 
-	# escuto
-	estadoDaComunicacao = escuto()
-	# se ok -> transmite controle
-	if estadoDaComunicacao == "silencio":
-		print(f"mensagem {contadorMensagens} de controle recebida")
-		contadorMensagens += 1
-		continue
-	# se não -> espera e tenta novamente mesma mensagem
-	else:
-		print(f"mensagem {contadorMensagens} de controle não recebida")
-		espera()
-		continue
+		# escuto
+		estadoDaComunicacao = escuto(probErroE=probErroE, probErroF=probErroF)
+		# se ok -> transmite controle
+		if estadoDaComunicacao == "silencio":
+			if imprime:
+				print(f"mensagem {contadorMensagens} de controle recebida")
+			contadorMensagens += 1
+			continue
+		# se não -> espera e tenta novamente mesma mensagem
+		else:
+			if imprime:
+				print(f"mensagem {contadorMensagens} de controle não recebida")
+			if esperar:
+				espera()
+			continue
+
+	return numeroErrosE, numeroErrosF
+
+if __name__ == "__main__":
+	totalMensagens = 1e3 # total de mensagens a serem transmitidas
+	probsErroE = [0.2] # probabilidade de mensagem ser corrompida
+	probsErroF = [0.2] # probabilidade de mensagem ser perdida
+	
+	numeroErrosE_ = []
+	numeroErrosF_ = []
+
+	for i in range(len(probsErroE)):
+		numeroErrosE, numeroErrosF = simulacao(totalMensagens, probsErroE[i], probsErroF[i])
+		numeroErrosE_.append(numeroErrosE)
+		numeroErrosF_.append(numeroErrosF)
+
+	print(f"numero de erros E: {numeroErrosE_}")
+	print(f"numero de erros F: {numeroErrosF_}")
